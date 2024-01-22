f61ae6277bc756aca4e11624792095d8e6bc20b5
tahafatmi1 <155869314+tahafatmi1@users.noreply.github.com>
2024-01-22 10:40:59 +0500

    Add files via upload

---
+++ JS1/script.js
@@ -0,0 +1,8 @@
+alert("Welcome to the website. This is made by Taha Fatmi for Bano Qabil Assignment.")
+
+var age = 20
+var isStudent = true
+
+console.log("Age",age);
+console.log("IS Student: ",isStudent);
+
