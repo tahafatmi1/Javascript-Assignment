134ba571203eac5339f794729d66d20e9a7c7dc9
tahafatmi1 <155869314+tahafatmi1@users.noreply.github.com>
2024-01-22 11:01:40 +0500

    Add files via upload

---
+++ JS Assignment 2/script.js
@@ -0,0 +1,11 @@
+var userName = prompt("What is your name?");
+
+alert("Hello, " + userName + "! Welcome to our website.");
+
+
+
+var length = 19
+var width = 45
+var area = length*width
+
+console.log("Area: ", area);
